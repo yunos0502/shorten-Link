f84b9e1a2dbf2bc14d02899d4b7243facd605a3f
Park Gyu Ae <yunos0502@gmail.com>
2021-05-09 21:49:50 +0900

    update build

---
+++ out/_next/static/80M6pybc8-PorvuqPz2ay/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d6a9f98a8443571cedba.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e84d56492380176374e0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
